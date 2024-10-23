ebb6e45e97ba78a2ee7dc4c5f441e6d9fc589480
soumyashreebehera <soumyashree161@gmail.com>
2024-10-23 18:34:44 +0530

    counter added

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -5,15 +5,32 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  function decreaseHandler(){
+    setCount(count-1);
+  }
+
+  function increaseHandler(){
+    setCount(count+1);
+  }
+
+  function resetHandler(){
+    setCount(0);
+  }
 
   return (
-    <>
-     
-      <h1 className='bg-green-400 text-black p-4 rounded-xl'>
-        tailwind test</h1>
-       
-    </>
-  )
+   <div className='w-[100vw] h-[100vh] flex justify-center items-center bg-slate-500 flex-col gap-10 '>
+    <div className='text-cyan-400 font-bold text-2xl'>Increment & Decrement</div>
+    <div className='bg-slate-100 flex justify-center gap-7 py-3 rounded-sm text-[10px] text-red-300'>
+      <button onClick={decreaseHandler} className='border-r-2 text-2xl  w-15 border-slate-800 '>-</button>
+      <div className='font-bold gap-12 text-5xl items-center'>
+      {count}
+      </div>
+      <button onClick={increaseHandler} className='border-l-2 text-2xl w-15 border-slate-800 '>+</button>
+      </div>
+      <button onClick={resetHandler} className='text-xl text-cyan-400 rounded-sm'>Reset</button>
+   
+   </div>
+  );
 }
 
 export default App
